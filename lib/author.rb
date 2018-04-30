b7a76d7e231d4f04a78a0c62563c5827e6a1bf05
Ji Yim <gsooyim@gmail.com>
2018-04-30 21:52:25 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name, :posts
+  @@post_counts = 0 
   
   def initialize(name)
     @name = name
@@ -9,12 +10,18 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_counts += 1
   end
   
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
+    @@post_counts += 1
+  end
+  
+  def self.post_count
+    @@post_counts
   end
   
 end
